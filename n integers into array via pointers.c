98252cd809574a936ef93e1bb6115a0aadfed226
ARIF KHAN <arifkhan80730@gmail.com>
2025-11-24 08:17:54 +0530

    Update n integers into array via pointers.c

--- n integers into array via pointers.c
+++ n integers into array via pointers.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
-// Input array using pointer arithmetic
 void input_array(int *arr, int n) {
     for (int i = 0; i < n; i++) {
         printf("Enter element %d: ", i + 1);
         scanf("%d", arr + i);
     }
 }
-
-// Display array using pointer arithmetic
 void display_array(int *arr, int n) {
     printf("Array elements: ");
     for (int i = 0; i < n; i++) {
@@ -16,8 +12,6 @@ void display_array(int *arr, int n) {
     }
     printf("\n");
 }
-
-// Sum array elements using pointer arithmetic
 int sumArray(int *arr, int n) {
     int sum = 0;
     for (int i = 0; i < n; i++) {
@@ -25,8 +19,6 @@ int sumArray(int *arr, int n) {
     }
     return sum;
 }
-
-// Count positive elements using pointer arithmetic
 int countPos(int *arr, int n) {
     int count = 0;
     for (int i = 0; i < n; i++) {
@@ -36,8 +28,6 @@ int countPos(int *arr, int n) {
     }
     return count;
 }
-
-// Count negative elements using pointer arithmetic
 int countNeg(int *arr, int n) {
     int count = 0;
     for (int i = 0; i < n; i++) {
@@ -49,25 +39,18 @@ int countNeg(int *arr, int n) {
 }
 
 int main() {
-    int arr[100], n;
-
-    // Input size
+    int arr[100],n;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
-
-    // Input and display
     input_array(arr, n);
     display_array(arr, n);
-
-    // Calculations
     int sum = sumArray(arr, n);
     int pos_c = countPos(arr, n);
     int neg_c = countNeg(arr, n);
-
-    // Output results
     printf("Sum of all elements: %d\n", sum);
     printf("Number of positive elements: %d\n", pos_c);
     printf("Number of negative elements: %d\n", neg_c);
 
     return 0;
 }
+
