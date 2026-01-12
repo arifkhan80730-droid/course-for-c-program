c54690cc831dd8cf26708c1c10ade38cf79acec8
ARIF KHAN <arifkhan80730@gmail.com>
2026-01-12 08:50:09 +0530

    c program which covers every concept
    writing a c program which covers every concept

--- main.c
+++ main.c
@@ -1,18 +1,33 @@
-#include<stdio.h>
-int main(){
-  int arr[5]={10,20,30,40,50};
-  int i,key,found=0;
-  printf("enter the element to search:");
-  scanf("%d ",&key);
-  for(i=0;i<5;i++){
-  if(arr[i]==key){
-    found=1;
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[30] = "HELLO";
+    char s2[30] = "WORLD";
+    int userchoice;
+
+    printf("choose option 1-Length, 2-Concatenate, 3-Compare and Copy: ");
+    scanf("%d", &userchoice);
+
+    switch (userchoice) {
+    case 1:
+    printf("Length of s1: %lu\n", strlen(s1));
+    strcat(s1, s2);
+    printf("After strcat: %s\n", s1);
     break;
-  }
-}
-   if(found)
-    printf("element%dfound at position %d\n",key,i+1);
-    else
-        printf("element %d not found\n",key);
+    case 2:
+    strcat(s1, s2);
+    printf("After concatenation: %s\n", s1);
+    strcpy(s1, "HELLO");
+    break;
+    case 3:
+    printf("Comparison result before copy: %d\n", strcmp(s1, s2));
+    strcpy(s1, s2);
+    printf("After copying s2 into s1: %s\n", s1);
+    printf("Comparison result after copy: %d\n", strcmp(s1, s2));
+    break;
+    default:
+    printf("Invalid choice!\n");
+    }
     return 0;
 }
